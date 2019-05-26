9458b7425f3a2829cd815b3577f430e1d3f63626
Eric Goldner <6527129+eg467@users.noreply.github.com>
2019-05-25 21:19:48 -0400

    Added jsonbin.io API support
    Replaces MyJSON Javascript API use.

---
+++ ApiProxy/ApiHttpClient.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace ApiProxy
+{
+    public class ApiHttpClient
+    {
+        private readonly HttpRequestMessage _request;
+
+        public ApiHttpClient(HttpRequestMessage request)
+        {
+            _request = request;
+        }
+
+        public async Task<HttpResponseMessage> PostAsync(string uri, IReadOnlyDictionary<string, string> headers, object requestBody)
+        {
+            return await MakeCall(headers, client => client.PostAsJsonAsync(uri, requestBody));
+        }
+
+        public async Task<HttpResponseMessage> PutAsync(string uri, IReadOnlyDictionary<string, string> headers, object requestBody)
+        {
+            return await MakeCall(headers, client => client.PutAsJsonAsync(uri, requestBody));
+        }
+
+        public async Task<HttpResponseMessage> DeleteAsync(string uri, IReadOnlyDictionary<string, string> headers)
+        {
+            return await MakeCall(headers, client => client.DeleteAsync(uri));
+        }
+
+        public async Task<HttpResponseMessage> GetAsync(string uri, IReadOnlyDictionary<string, string> headers)
+        {
+            return await MakeCall(headers, client => client.DeleteAsync(uri));
+        }
+
+        private HttpResponseMessage CreateErrorResponse(HttpStatusCode code, Exception exception)
+        {
+#if DEBUG
+            return _request.CreateErrorResponse(code, exception);
+#else
+            // Hide raw responses since they sometimes (especially JsonBin) return sensitive API keys.
+            return _request.CreateErrorResponse(code, "There was an error processing your request.");
+#endif
+        }
+
+        private async Task<HttpResponseMessage> MakeCall(IReadOnlyDictionary<string, string> headers, Func<HttpClient, Task<HttpResponseMessage>> httpCall)
+        {
+            try
+            {
+                var client = CreateClientWithHeaders();
+                var response = await httpCall(client);
+                await EnsureSuccessfulResponse(response);
+                var responseBody = ReadResponseBody(response);
+                return _request.CreateResponse(HttpStatusCode.OK, responseBody);
+            }
+            catch (HttpException exc)
+            {
+                return CreateErrorResponse((HttpStatusCode)exc.GetHttpCode(), exc);
+            }
+            catch (Exception exc)
+            {
+                return CreateErrorResponse(HttpStatusCode.InternalServerError, exc);
+            }
+
+            HttpClient CreateClientWithHeaders()
+            {
+                var client = new HttpClient();
+                client.DefaultRequestHeaders.Accept.Clear();
+
+                headers
+                    .ToList()
+                    .ForEach(kv => client.DefaultRequestHeaders.Add(kv.Key, kv.Value));
+                return client;
+            }
+
+            async Task EnsureSuccessfulResponse(HttpResponseMessage response)
+            {
+                if (!response.IsSuccessStatusCode)
+                {
+                    var content = await response.Content.ReadAsStringAsync();
+                    throw new HttpException((int)response.StatusCode, content);
+                }
+            }
+        }
+
+        public async Task<object> ReadResponseBody(HttpResponseMessage response)
+        {
+            return await response.Content.ReadAsAsync<object>();
+        }
+    }
+}
---
+++ ApiProxy/Controllers/JsonbinProxyController.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections.Generic;
+using System.Configuration;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace ApiProxy.Controllers
+{
+    [RoutePrefix("api/jsonbin")]
+    public class JsonbinController : ApiController
+    {
+        private ApiHttpClient Client => new ApiHttpClient(Request);
+        private const string BaseUri = "https://api.jsonbin.io/b";
+        private static string UriFromBinId(string binId) => $"{BaseUri}/{binId}";
+        private static string SecretKey => ConfigurationManager.AppSettings["JsonbinSecretKey"];
+        private const string BookmarkCollectionKey = "5ce92640b4565f1948046cb9";
+
+        private Dictionary<string, string> BaseHeaders() =>
+            new Dictionary<string, string>
+            {
+                {"secret-key", SecretKey}
+            };
+
+        [HttpGet]
+        [Route("b/{id}", Name = "GetBin")]
+        public async Task<object> GetBin(string id)
+        {
+            return await Client.GetAsync(UriFromBinId(id), BaseHeaders());
+        }
+
+        [HttpPost]
+        [Route("b", Name = "CreateBin")]
+        public async Task<object> CreateBin([FromBody]object data, bool @private = true)
+        {
+            var headers = BaseHeaders();
+            headers.Add("collection-id", BookmarkCollectionKey);
+            headers.Add("private", @private.ToString());
+            return await Client.PostAsync(BaseUri, BaseHeaders(), data);
+        }
+
+        [HttpPut]
+        [Route("b/{id}", Name = "UpdateBin")]
+        public async Task<object> UpdateBin(string id, [FromBody]object data)
+        {
+            var headers = BaseHeaders();
+            headers.Add("versioning", "false");
+            return await Client.PutAsync(UriFromBinId(id), BaseHeaders(), data);
+        }
+
+        [HttpDelete]
+        [Route("b/{id}", Name = "DeleteBin")]
+        public async Task<object> DeleteBin(string id)
+        {
+            return await Client.DeleteAsync(UriFromBinId(id), BaseHeaders());
+        }
+
+
+    }
+}
